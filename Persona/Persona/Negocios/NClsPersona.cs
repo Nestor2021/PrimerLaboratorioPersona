ad473d5e800c43ea870de86f35d0397a1433ec28
Nestor2021 <87671739+Nestor2021@users.noreply.github.com>
2021-08-16 16:08:35 -0600

    NClsPersona

--- Persona/Persona/Negocios/NClsPersona.cs
+++ Persona/Persona/Negocios/NClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,35 @@ namespace Persona.Negocio
 {
     class NClsPersona
     {
+        public String Hablar(ClsPersona persona)
+        {
+            return "La persona " + persona.Nombre + " esta hablando";        
+  
+        }
+
+        public String Reir(ClsPersona persona)
+        {
+            return  persona.Nombre + " se esta riendo";
+        }
+
+        public String Caminar(ClsPersona persona)
+        {
+            return persona.Nombre + " esta caminando por el parque";
+        }
+
+        public String Observar(ClsPersona persona)
+        {
+            return persona.Nombre + " esta observando el partido";
+        }
+
+        public String Cantar(ClsPersona persona)
+        {
+            return persona.Nombre + " esta cantando en el escenario";
+        }
+
+        public String Saltar(ClsPersona persona)
+        {
+            return persona.Nombre + " esta jugando en el saltacuerda";
+        }
     }
 }
