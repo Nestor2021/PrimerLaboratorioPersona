69588bf4a9d11a9890702ca81f7c1806300f5481
AlexiVides <88692360+AlexiVides@users.noreply.github.com>
2021-08-16 16:00:09 -0600

    Entidades

--- Persona/Persona/Entidades/ClsPersona.cs
+++ Persona/Persona/Entidades/ClsPersona.cs
@@ -8,5 +8,24 @@ namespace Persona.Entidades
 {
     class ClsPersona
     {
+        private String nombre;
+        private String apellido;
+        private int edad;
+        private String sexo;
+        private int altura;
+        private String nacionalidad;
+        private String estado;
+        private String dui;
+        private int peso;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public int Altura { get => altura; set => altura = value; }
+        public string Nacionalidad { get => nacionalidad; set => nacionalidad = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Dui { get => dui; set => dui = value; }
+        public int Peso { get => peso; set => peso = value; }
     }
 }
